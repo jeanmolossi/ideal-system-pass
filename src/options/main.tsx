d77aa00fa096171262ba218baf647f15d886f196
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-08-21 20:10:03 -0300

    fix: load Tailwind via stylesheet (#13)

--- src/options/main.tsx
+++ src/options/main.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Options from './App';
 
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.classList.add('dark');
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Options />
